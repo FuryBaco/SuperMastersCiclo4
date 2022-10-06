d23a9a16d5f7c0f17f93d44e4caafac27b3a0b61
Jesus <104527491+FuryBaco@users.noreply.github.com>
2022-10-06 06:40:35 -0500

    Update server.js
    Configuracion para conectarse al servidor con el comando en terminal node back/server.js

--- back/server.js
+++ back/server.js
@@ -1,1 +1,12 @@
 const app = require('./app');
+
+// Language: javascript
+// Path: back\server.js
+// Set the configuration for the server
+
+const dotenv = require('dotenv');
+dotenv.config({ path: 'back/config/config.env' });
+
+const server= app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT} in mode ${process.env.NODE_ENV}.`);
+});
